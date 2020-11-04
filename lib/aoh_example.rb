c4b22c451d7dd6872c2bbbbe861fab78f3656d68
Jacob Tobias <jtobias811@gmail.com>
2020-11-04 19:46:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,13 +7,7 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
-  assembled_aoh = {
-    DON-G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
-  }
+  assembled_aoh = {DON-G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G}
 end
 
 def literal_aoh
